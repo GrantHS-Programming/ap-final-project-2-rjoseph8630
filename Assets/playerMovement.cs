7db6405e5b04866b72b13234fcd67c62f82ea702
rjoseph8630 <rjoseph8630@student.pps.net>
2023-05-18 14:02:21 -0700

    movemnt

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float dirX = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(dirX * 5f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(0, 10f);
+        }
     }
 }
